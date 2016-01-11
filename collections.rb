a3ebf03cf08ee6b0aeb9eea2233cd7cd82e5e5ac
Julian Guterman <gutermanj@gmail.com>
2016-01-11 12:27:07 -0500

    committing

--- collections.rb
+++ collections.rb
@@ -5,3 +5,13 @@ shoe_brands = [ "Nike", "Reebok", "Puma", "Steve Madden", "uggs", ]
 	shoe_brands.each_with_index do | brand, i |
 		puts "This brand is my No. #{i + 1} favorite: #{brand}"
 	end
+
+	favorite = shoe_brands.find do | brand |
+		result = brand.length == 6
+		puts brand + " -> #{result}"
+
+		result
+		#return true or false for brand
+	end
+
+	puts "my REAL favorite brand is: #{favorite}"
