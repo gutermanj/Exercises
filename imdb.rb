dd0c68b49539039c55208f6071702af635c098b3
Julian Guterman <gutermanj@gmail.com>
2016-01-14 10:13:01 -0500

    Commented movie exercise

--- imdb.rb
+++ imdb.rb
@@ -1,7 +1,7 @@
 
 require 'colorize'
 
-
+	#Handles the movie iteself
 class Movie
 	attr_accessor :title, :rating
 	def initialize(title, rating)
@@ -15,6 +15,8 @@ class NewRating
 		@movies = []
 		@max_rating = 10
 
+			# Reads the txt file with the movies in it and 
+			# pushes them into @movies array
 		file = File.open("movies.txt", "r")
 		  while (title = file.gets)
 		  	rating = rand(0...@max_rating)
@@ -26,6 +28,8 @@ class NewRating
 		file.close
 	end
 
+		# Keeps adding | # | as long as the rating (r) is less than the
+		# randomized @max_rating
 	def show_rating
 		r = @max_rating
 
@@ -46,21 +50,23 @@ class NewRating
 			r -= 1
 		end
 
-		(1..@movies.length).each{
+		# prints "--" throughout the length of the @movies array
+		(1..@movies.length).each do
 			print "--"
-		}
+		end
 		
 		puts "-"
 
-		(1..@movies.length).each_with_index {
-			| i |
+				# prints a "|" and the current index
+		(1..@movies.length).each_with_index do	| i |
 			print "|"
 			print "#{i}".colorize(:red)
-		}
+		end
 
 		puts "|"
 		puts ""
 
+			# prints the list of movies
 		@movies.each_with_index {
 			| movie, i |
 
