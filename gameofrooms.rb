5b1aa323fa7da878590620832cce13bb607f166e
Julian Guterman <gutermanj@gmail.com>
2016-01-13 23:51:23 -0500

    Finished game of rooms and fixed blog bug

--- gameofrooms.rb
+++ gameofrooms.rb
@@ -1,6 +1,7 @@
 class Game
 	attr_accessor :rooms, :current_pos
 	def initialize
+		@health = 100
 		@rooms = [] #Sets the array of rooms to nil as of now
 		@current_pos = 0 #This is used to determine which room you're in
 	end
@@ -14,35 +15,50 @@ class Game
 		puts "#{rooms}" #I made this to make sure the .add_room method worked
 	end
 
-	def start # This is complicated. This is what is going to determine if you go through the right exit
+	def finish
+		if @current_pos == @rooms.length - 1
+			true
+		else
+			false
+		end
+	end
 
-		puts "Theres light to the north" #The initial room you are at @rooms[0]
-			@rooms.each do | x | #looping through the array
-				answer = gets.chomp #Takes the input
+	def health
+		if @health == 0
+			true
+		else
+			false
+		end
+	end
 
-			if answer.upcase == @rooms[@current_pos].exit #If the input is equal to the exit (answer) of @rooms[@current_pos] <= That's why I set @current_pos to 0
-				if @current_pos == 4 #The last room
-					puts "Good Job M8 You Did It :D" #Congradulating the player!
-					
-				else
-				@current_pos += 1 #If you're not in the last room but your answer is == to @rooms[@current_pos] (reference line 23)
-				puts @rooms[@current_pos].description #It's going to add a position i.e. turn into @rooms[1] from @rooms[0] and it will print the new description of that @rooms item
-				end
-			else
-				puts "You hit a wall. You dead now. Sorry m8." #If it's the wrong answer it will simply print this and exit
-				exit
-				
-			end
+	def start(direction) # This is complicated. This is what is going to determine if you go through the right exit
+			current_room = @rooms[@current_pos]
+		if current_room.exit == direction
+			@current_pos += 1
+ 			new_room = @rooms[@current_pos]
+			puts new_room.description
+		elsif current_room.back == direction
+			@current_pos -= 1
+			new_room = @rooms[@current_pos]
+			puts new_room.description
+		else
+			puts "You hit a wall, try again."
+			@health -= 25
+			puts "Your health is now at #{@health}%"
+		end
+		if @health == 0
+			puts "You are dead... Game Over."
 		end
 	end
 end
 
 
 class Room
-	attr_accessor :description, :exit #This class is simply so you can create the actual rooms and manually put them into the @rooms array
-	def initialize(description, exit) #The 2 different details about them (exit == answer)
+	attr_accessor :description, :exit, :back #This class is simply so you can create the actual rooms and manually put them into the @rooms array
+	def initialize(description, exit, back) #The 2 different details about them (exit == answer)
 		@description = description #Making them accessible
 		@exit = exit #Same as 44
+		@back = back
 	end
 
 end
@@ -50,11 +66,11 @@ end
 room_adder = Game.new #This is so in a way I can use the Game class
 
 
-room = Room.new("There is a light to the north","N") #These are the actualy room objects I'm creating
-room2 = Room.new("There is a light to the east", "E")
-room3 = Room.new("There is a light to the south", "S")
-room4 = Room.new("There is a light to the east", "E")
-room5 = Room.new("There is a light to the north", "N")
+room = Room.new("There is a light to the north","N", "N") #These are the actual room objects I'm creating with different perameters
+room2 = Room.new("There is a light to the east", "E", "S")
+room3 = Room.new("There is a light to the south", "S", "W")
+room4 = Room.new("There is a light to the east", "E", "N")
+room5 = Room.new("You did it, good job m8!", "N", "W")
 
 
 room_adder.add_room(room) #room_adder is = Game.New so I am using the method .add_room which will push the Room.new Object into the array in class Game.
@@ -63,4 +79,12 @@ room_adder.add_room(room3)
 room_adder.add_room(room4)
 room_adder.add_room(room5)
 
-room_adder.start #room_adder again so I'm using Game class and using the method start which handles how the game is played (reference line 17)
+		input = ""
+
+		puts "There's a light to the North."
+
+while input != "EXIT" && room_adder.finish == false && room_adder.health == false
+	input = gets.chomp.upcase
+	room_adder.start(input)
+end
+ #room_adder again so I'm using Game class and using the method start which handles how the game is played (reference line 17)
