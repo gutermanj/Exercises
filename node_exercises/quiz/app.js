31f3b0cc045ce8967c3f66970d3249f690ecee59
Julian Guterman <gutermanj@gmail.com>
2016-01-30 15:25:32 -0500

    Finished node quiz exercise

--- node_exercises/quiz/app.js
+++ node_exercises/quiz/app.js
@@ -7,14 +7,20 @@ var Question = function(id, question, answer, points) {
 	this.question = question;
 	this.answer = answer;
 	this.points = points;
-
 }
+
+var numberone = Math.floor(Math.random() * 11);
+
+var numbertwo = Math.floor(Math.random() * 11);
+
+var randomAnswer = numberone * numbertwo;
 
 var question1 = new Question(1, "What's 2 + 2?", '4', 2)
 var question2 = new Question(2, "What's 5 x 10?", '50', 6)
 var question3 = new Question(3, "What's 30 / (5 + 10)?", '2', 8)
+var bonus = new Question(4, "What is " + numberone + "x" + numbertwo + "?", `${randomAnswer}`, 50)
 
-var quiz = new Quiz([question1, question2, question3]);
+var quiz = new Quiz([question1, question2, question3, bonus]);
 
 quiz.startgame();
 
--- node_exercises/quiz/quizclass.js
+++ node_exercises/quiz/quizclass.js
@@ -22,11 +22,18 @@ Quiz.prototype.checkAnswer = function (err, input) {
 			throw error;
 		}
 		// Validates the input compared to correct answer, and checks if there are any questions left
-		else if (input === this.currentQuestion.answer) {
+		else if (input === this.currentQuestion.answer ) {
 				if (this.position === this.questionArray.length - 1) {
 					this.score += this.currentQuestion.points;
 					console.log('You finished with a score of: ' + this.score);
 					console.log('You win!');
+				}
+				else if (this.position === this.questionArray.length - 2) {
+					this.score += this.currentQuestion.points;
+					this.position += 1;
+					this.currentQuestion = this.questionArray[this.position]
+					console.log('Bonus Question! Answer this correctly for 50 points!');
+					this.startgame();
 				}
 					else {
 					this.score += this.currentQuestion.points;
