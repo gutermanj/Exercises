a241b77447858b1e0f4a0a3bdc34317c04a7f22d
Julian Guterman <gutermanj@gmail.com>
2016-02-14 17:35:27 -0500

    Created partial template for profile. Users can sign up, edit profile, upload pictures, and authenticate.

---
+++ rails/aimuzer/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  	def show
+  		@user = User.find(params[:id])
+  		@zipcode = ZipCodes.identify(@user.zipcode.to_s)
+	end
+
+	def new
+	end
+
+	def create
+	  @user = User.create( user_params )
+	end
+
+	def destroy
+		@user.avatar = nil
+		@user.save
+	end
+
+	private
+
+	# Use strong_parameters for attribute whitelisting
+	# Be sure to update your create() and update() controller methods.
+
+	def user_params
+	  params.require(:user).permit(:avatar)
+	end
+
+end
+
+
