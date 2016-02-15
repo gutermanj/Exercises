2f20c0596453456bddb51c30b00f0a8831fa2650
Julian Guterman <gutermanj@gmail.com>
2016-02-14 19:12:57 -0500

    created template for profile, as well as personal info bar

--- rails/aimuzer/app/controllers/users_controller.rb
+++ rails/aimuzer/app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   	def show
   		@user = User.find(params[:id])
   		@zipcode = ZipCodes.identify(@user.zipcode.to_s)
+
+  		@randtags = rand(100)
+  		@randfollowers = rand(1000)
+
 	end
 
 	def new
